a65b081f2b0a79925446360d1975d4bde20bbbc0
Brixter Porras <60214966+Brix101@users.noreply.github.com>
2023-08-14 10:43:11 +0800

    Update bufferline.lua

--- .config/nvim/after/plugin/bufferline.lua
+++ .config/nvim/after/plugin/bufferline.lua
@@ -3,4 +3,11 @@ if not status_ok then
 	return
 end
 
-bufferline.setup({})
+bufferline.setup({
+	options = {
+		max_name_length = 30,
+		max_prefix_length = 30, -- prefix used when a buffer is de-duplicated
+		tab_size = 21,
+		offsets = { { filetype = "neo-tree", text = "File Explorer", text_align = "center", separator = true } },
+	},
+})
