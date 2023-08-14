b7505e009f9aca1c50990421c6c4ddf55dc698c9
Brixter Porras <60214966+Brix101@users.noreply.github.com>
2023-08-14 11:08:16 +0800

    Update keymaps.lua

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -48,7 +48,7 @@ keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string u
 keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
 keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
 
--- telescope git commands (not on youtube nvim video)
+-- telescope git commands
 keymap.set("n", "<leader>gc", "<cmd>Telescope git_commits<cr>") -- list all git commits (use <cr> to checkout) ["gc" for git commits]
 keymap.set("n", "<leader>gfc", "<cmd>Telescope git_bcommits<cr>") -- list git commits for current file/buffer (use <cr> to checkout) ["gfc" for git file commits]
 keymap.set("n", "<leader>gb", "<cmd>Telescope git_branches<cr>") -- list git branches (use <cr> to checkout) ["gb" for git branch]
@@ -56,3 +56,12 @@ keymap.set("n", "<leader>gs", "<cmd>Telescope git_status<cr>") -- list current c
 
 -- restart lsp server (not on youtube nvim video)
 keymap.set("n", "<leader>rs", ":LspRestart<CR>") -- mapping to restart lsp if necessary
+
+--bufferline
+keymap.set("n", "<C-w>", "<Cmd>BufferLinePick<CR>") -- close current buffer with ctrl-w
+-- keymap.set("n", "<leader><tab>", "<Cmd>BufferLineCycleNext<CR>")
+-- keymap.set("n", "<S-tab>", "<Cmd>BufferLineCyclePrev<CR>")
+-- keymap.set("n", "[b", "<Cmd>BufferLineMoveNext<CR>")
+-- keymap.set("n", "]b", "<Cmd>BufferLineMovePrev<CR>")
+keymap.set("n", "[b", "<Cmd>BufferLineCycleNext<CR>")
+keymap.set("n", "]b", "<Cmd>BufferLineCyclePrev<CR>")
