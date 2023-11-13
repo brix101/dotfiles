13d7bb8b9bea679b36f0d2d439ea95884b2c8329
Brix <brixterporras@gmail.com>
2023-11-13 16:14:51 +0800

    feat: added go imports

--- .config/nvim/lua/brix/plugins/indent-blankline.lua
+++ .config/nvim/lua/brix/plugins/indent-blankline.lua
@@ -2,6 +2,10 @@ return {
 	"lukas-reineke/indent-blankline.nvim",
 	main = "ibl",
 	config = function()
+		-- vim.opt.list = true
+		vim.opt.listchars:append("space:⋅")
+		vim.opt.listchars:append("eol:↴")
+
 		local highlight = {
 			"RainbowRed",
 			"RainbowYellow",
--- .config/nvim/lua/brix/plugins/lsp.lua
+++ .config/nvim/lua/brix/plugins/lsp.lua
@@ -272,6 +272,14 @@ return {
 			lsp_cfg = true,
 			lsp_on_attach = on_attach,
 		})
+		local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			pattern = "*.go",
+			callback = function()
+				require("go.format").goimport()
+			end,
+			group = format_sync_grp,
+		})
 
 		require("rust-tools").setup({ server = rust_lsp })
 
