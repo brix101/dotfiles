ebd37997550704a1f75bd0cb98956ac820524e31
Brixter Porras <60214966+Brix101@users.noreply.github.com>
2023-11-29 10:46:13 +0800

    Update init.lua

--- .config/nvim/lua/brix/init.lua
+++ .config/nvim/lua/brix/init.lua
@@ -1,7 +1,3 @@
-require("brix.autocmds")
-require("brix.options")
-require("brix.keymaps")
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({
