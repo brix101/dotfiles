d9e1be8539faed571307b63eb8f2b3c44348d43e
Brixter Porras <brixterporras@gmail.com>
2023-12-04 23:20:19 +0800

    style: waybar

--- .config/waybar/scripts/updates.sh
+++ .config/waybar/scripts/updates.sh
@@ -2,14 +2,9 @@
 
 UPDATES="$(($(pacman -Qu | wc -l) + $(paru -Qu | wc -l)))"
 echo "$UPDATES"
-# if [[ ${UPDATES} != "0" ]]
-# then
-# 	notify-send -a "openSUSE TumbleWeed Updates" "Total number of packages to update" "$UPDATES"
-# else
-# 	notify-send -a "openSUSE TumbleWeed Updates" "Up to date" "No packages need to be upgraded"
-# fi
-# if [[ $updates -gt 0 ]]; then
-#     echo "#$updates"
-# else
-#     echo ""
-# fi
+if [[ ${UPDATES} != "0" ]]; then
+    notify-send -a "Arch Linux Updates" "Total number of packages to update" "$UPDATES"
+else
+    notify-send -a "Arch Linux Updates" "Up to date" "No packages need to be upgraded"
+fi
+
