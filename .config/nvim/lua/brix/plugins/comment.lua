f4f19b1a51aa494f2882401d1167cf6fdea832ff
Brixter Porras <brixterporras@gmail.com>
2024-03-04 04:22:50 +0800

    fix: deprication error on comment string

--- .config/nvim/lua/brix/plugins/comment.lua
+++ .config/nvim/lua/brix/plugins/comment.lua
@@ -5,15 +5,15 @@ return {
 		"JoosepAlviste/nvim-ts-context-commentstring",
 	},
 	config = function()
-		-- import comment plugin safely
-		local comment = require("Comment")
+		require("ts_context_commentstring").setup({
+			enable_autocmd = false,
+			languages = {
+				typescript = "// %s",
+			},
+		})
 
-		local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
-
-		-- enable comment
-		comment.setup({
-			-- for commenting tsx and jsx files
-			pre_hook = ts_context_commentstring.create_pre_hook(),
+		require("Comment").setup({
+			pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
 			opts = {
 				-- ignores empty lines
 				ignore = "^$",
