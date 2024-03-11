98f88f77e5d176f05e870dfeda315db0bf7810d4
Brixter Porras <brixterporras@gmail.com>
2024-03-11 19:55:04 +0800

    fix: comment ts

--- .config/nvim/lua/brix/plugins/comment.lua
+++ .config/nvim/lua/brix/plugins/comment.lua
@@ -5,9 +5,11 @@ return {
 		"JoosepAlviste/nvim-ts-context-commentstring",
 		"nvim-treesitter/nvim-treesitter",
 	},
-	opts = {
-		pre_hook = function(...)
-			require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()(...)
-		end,
-	},
+	config = function()
+		local prehook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()
+
+		require("Comment").setup({
+			pre_hook = prehook,
+		})
+	end,
 }
