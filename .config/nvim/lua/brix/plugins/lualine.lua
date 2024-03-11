bc51694bc21f08667aca5845aa201055abe00629
Brixter Porras <60214966+Brix101@users.noreply.github.com>
2024-03-11 10:36:32 +0800

    Update lualine.lua

--- .config/nvim/lua/brix/plugins/lualine.lua
+++ .config/nvim/lua/brix/plugins/lualine.lua
@@ -1,20 +1,18 @@
 return {
-	{
-		"nvim-lualine/lualine.nvim",
-		event = "VimEnter",
-		config = function()
-			local lualine = require("lualine")
+	"nvim-lualine/lualine.nvim",
+	event = "VimEnter",
+	config = function()
+		local lualine = require("lualine")
 
-			lualine.setup({
-				options = {
-					theme = "auto",
-					component_separators = { left = "", right = "" },
-					section_separators = { left = "", right = "" },
-				},
-				sections = {
-					lualine_c = {},
-				},
-			})
-		end,
-	},
+		lualine.setup({
+			options = {
+				theme = "auto",
+				component_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
+			},
+			sections = {
+				lualine_c = {},
+			},
+		})
+	end,
 }
