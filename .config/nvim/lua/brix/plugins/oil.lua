9eeee6d545b20851a45b11825446296f2ffb2f8c
Brixter Porras <brixterporras@gmail.com>
2024-03-11 20:11:23 +0800

    feat: added oil config

--- .config/nvim/lua/brix/plugins/oil.lua
+++ .config/nvim/lua/brix/plugins/oil.lua
@@ -17,6 +17,24 @@ return {
 			keymaps_help = {
 				border = "rounded",
 			},
+			view_options = {
+				-- Show files and directories that start with "."
+				show_hidden = true,
+				-- This function defines what is considered a "hidden" file
+				is_hidden_file = function(name, bufnr)
+					return vim.startswith(name, ".")
+				end,
+				-- This function defines what will never be shown, even when `show_hidden` is set
+				is_always_hidden = function(name, bufnr)
+					return false
+				end,
+				sort = {
+					-- sort order can be "asc" or "desc"
+					-- see :help oil-columns to see which columns are sortable
+					{ "type", "asc" },
+					{ "name", "asc" },
+				},
+			},
 			-- Configuration for the floating window in oil.open_float
 			float = {
 				-- Padding around the floating window
@@ -32,6 +50,31 @@ return {
 				override = function(conf)
 					return conf
 				end,
+			},
+			-- Configuration for the actions floating preview window
+			preview = {
+				-- Width dimensions can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+				-- min_width and max_width can be a single value or a list of mixed integer/float types.
+				-- max_width = {100, 0.8} means "the lesser of 100 columns or 80% of total"
+				max_width = 0.9,
+				-- min_width = {40, 0.4} means "the greater of 40 columns or 40% of total"
+				min_width = { 40, 0.4 },
+				-- optionally define an integer/float for the exact width of the preview window
+				width = nil,
+				-- Height dimensions can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+				-- min_height and max_height can be a single value or a list of mixed integer/float types.
+				-- max_height = {80, 0.9} means "the lesser of 80 columns or 90% of total"
+				max_height = 0.9,
+				-- min_height = {5, 0.1} means "the greater of 5 columns or 10% of total"
+				min_height = { 5, 0.1 },
+				-- optionally define an integer/float for the exact height of the preview window
+				height = nil,
+				border = "rounded",
+				win_options = {
+					winblend = 0,
+				},
+				-- Whether the preview window is automatically updated when the cursor is moved
+				update_on_cursor_moved = true,
 			},
 		})
 		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
