296419088977ddbafc4fd8a907bbc87f6f3723b7
Brixter Porras <60214966+Brix101@users.noreply.github.com>
2024-03-11 11:20:46 +0800

    Update comment.lua

--- .config/nvim/lua/brix/plugins/comment.lua
+++ .config/nvim/lua/brix/plugins/comment.lua
@@ -2,17 +2,12 @@ return {
 	"numToStr/Comment.nvim",
 	event = { "BufReadPre", "BufNewFile" },
 	dependencies = {
-		{
-			"JoosepAlviste/nvim-ts-context-commentstring",
-			ft = { "typescriptreact" },
-			opts = {
-				enable_autocmd = false,
-			},
-		},
+		"JoosepAlviste/nvim-ts-context-commentstring",
+		"nvim-treesitter/nvim-treesitter,
 	},
 	opts = {
-		pre_hook = function(ctx)
-			return require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()(ctx)
+		pre_hook = function()		
+        		return vim.bo.commentstring
 		end,
 	},
 }
