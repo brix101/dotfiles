12dbd9e2ea10427aa7dc5f00cf61b1f8e8242992
Brixter Porras <brixterporras@gmail.com>
2025-01-21 09:42:46 +0800

    feat: added eslint fomratting

--- .config/nvim/lua/plugins/lang/typescript.lua
+++ .config/nvim/lua/plugins/lang/typescript.lua
@@ -174,6 +174,15 @@ return {
           opts.settings.javascript =
             vim.tbl_deep_extend("force", {}, opts.settings.typescript, opts.settings.javascript or {})
         end,
+        eslint = function()
+          require("lazyvim.util").lsp.on_attach(function(client)
+            if client.name == "eslint" then
+              client.server_capabilities.documentFormattingProvider = true
+            elseif client.name == "tsserver" then
+              client.server_capabilities.documentFormattingProvider = false
+            end
+          end)
+        end,
       },
     },
   },
