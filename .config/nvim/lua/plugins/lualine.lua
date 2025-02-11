995c8a71064758f34f4aa79b473a92ec4caf1598
Brixter Porras <brixterporras@gmail.com>
2025-02-11 18:54:50 +0800

    style: lua line

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -89,20 +89,6 @@ return {
 
       vim.o.laststatus = vim.g.lualine_laststatus
 
-      local function copilot_status()
-        local clients = package.loaded["copilot"] and vim.lsp.get_clients({ name = "copilot", bufnr = 0 }) or {}
-        if #clients > 0 then
-          local status = require("copilot.api").status.data.status
-          return (status == "InProgress" and "pending") or (status == "Warning" and "error") or "ok"
-        end
-      end
-
-      local copilot_colors = {
-        ok = "Special",
-        error = "DiagnosticError",
-        pending = "DiagnosticWarn",
-      }
-
       local opts = {
         options = {
           theme = "auto",
@@ -132,12 +118,6 @@ return {
           },
           lualine_x = {
             Snacks.profiler.status(),
-            -- stylua: ignore
-            {
-              function() return icons.kinds.Copilot end,
-              cond = function() return copilot_status() ~= nil end,
-              color = function() return { fg = Snacks.util.color(copilot_colors[copilot_status()] or copilot_colors.ok) } end,
-            },
             -- stylua: ignore
             {
               function() return require("noice").api.status.command.get() end,
@@ -209,6 +189,32 @@ return {
         symbols and symbols.get,
         cond = function()
           return vim.b.trouble_lualine ~= false and symbols.has()
+        end,
+      })
+
+      local function copilot_status()
+        local clients = package.loaded["copilot"] and vim.lsp.get_clients({ name = "copilot", bufnr = 0 }) or {}
+        if #clients > 0 then
+          local status = require("copilot.api").status.data.status
+          return (status == "InProgress" and "pending") or (status == "Warning" and "error") or "ok"
+        end
+      end
+
+      local copilot_colors = {
+        ok = "Special",
+        error = "DiagnosticError",
+        pending = "DiagnosticWarn",
+      }
+
+      table.insert(opts.sections.lualine_x, 2, {
+        function()
+          return icons.kinds.Copilot
+        end,
+        cond = function()
+          return copilot_status() ~= nil
+        end,
+        color = function()
+          return { fg = Snacks.util.color(copilot_colors[copilot_status()] or copilot_colors.ok) }
         end,
       })
 
