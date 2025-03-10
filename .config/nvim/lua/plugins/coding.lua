8deae1aef64f5d8e059757a3ffde4169eeb61798
Brixter Porras <brixterporras@gmail.com>
2025-03-10 10:16:31 +0800

    refactor: blink cmp

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -63,18 +63,4 @@ return {
       -- end)
     end,
   },
-
-  {
-    "folke/lazydev.nvim",
-    ft = "lua",
-    cmd = "LazyDev",
-    opts = {
-      library = {
-        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-        { path = "LazyVim", words = { "LazyVim" } },
-        { path = "snacks.nvim", words = { "Snacks" } },
-        { path = "lazy.nvim", words = { "LazyVim" } },
-      },
-    },
-  },
 }
