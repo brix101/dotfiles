1f9efd56d02cd71808753fb72fc61b8d672d98a2
Brixter Porras <brixterporras@gmail.com>
2025-06-14 14:40:10 +0800

    fix: redudunt config

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -61,14 +61,6 @@ return {
         ghost_text = {
           enabled = vim.g.ai_cmp,
         },
-        list = {
-          selection = {
-            preselect = function()
-              return not require("blink.cmp").snippet_active({ direction = 1 })
-            end,
-            auto_insert = true,
-          },
-        },
       },
       signature = {
         enabled = true,
@@ -95,36 +87,7 @@ return {
       },
       keymap = {
         preset = "super-tab",
-        ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
-        ["<C-e>"] = { "hide", "fallback" },
         ["<CR>"] = { "accept", "fallback" },
-        ["<Tab>"] = {
-          -- stylua: ignore
-          function(cmp)
-            local copilot = require("copilot.suggestion")
-
-            if vim.g.ai_cmp and copilot.is_visible() then
-              copilot.accept()
-              return true
-            elseif cmp.snippet_active() then
-              return cmp.accept()
-            else
-              return cmp.select_and_accept()
-            end
-          end,
-          "snippet_forward",
-          "fallback",
-        },
-        ["<S-Tab>"] = { "snippet_backward", "fallback" },
-        ["<Up>"] = { "select_prev", "fallback" },
-        ["<Down>"] = { "select_next", "fallback" },
-
-        ["<C-p>"] = { "select_prev", "fallback" },
-        ["<C-n>"] = { "select_next", "fallback" },
-
-        ["<C-b>"] = { "scroll_documentation_up", "fallback" },
-        ["<C-f>"] = { "scroll_documentation_down", "fallback" },
-        ["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
       },
     },
   },
