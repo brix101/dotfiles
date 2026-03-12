4290bf1f487ad358105326ed351cb6777c1730d3
Brixter Porras <brixterporras@gmail.com>
2026-03-12 22:54:03 +0800

    update keymaps

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -12,31 +12,71 @@ return {
   keys = function()
     local keys = {
       {
-        "<leader>H",
+        "<leader>ha",
         function()
           require("harpoon"):list():add()
         end,
-        desc = "Harpoon File",
+        desc = "add file",
       },
       {
-        "<leader>h",
+        "<leader>hl",
         function()
           local harpoon = require("harpoon")
           harpoon.ui:toggle_quick_menu(harpoon:list())
         end,
-        desc = "Harpoon Quick Menu",
+        desc = "quick menu",
+      },
+      {
+        "<leader>hr",
+        function()
+          require("harpoon"):list():remove()
+        end,
+        desc = "remove file",
+      },
+      {
+        "<leader>hc",
+        function()
+          require("harpoon"):list():clear()
+        end,
+        desc = "clear files",
       },
     }
 
-    for i = 1, 9 do
+    for i = 1, 4 do
       table.insert(keys, {
         "<leader>" .. i,
         function()
           require("harpoon"):list():select(i)
         end,
-        desc = "Harpoon to File " .. i,
+        desc = "harpoon " .. i,
       })
     end
     return keys
+  end,
+  config = function()
+    local harpoon = require("harpoon")
+
+    local conf = require("telescope.config").values
+    local function toggle_telescope(harpoon_files)
+      local file_paths = {}
+      for _, item in ipairs(harpoon_files.items) do
+        table.insert(file_paths, item.value)
+      end
+
+      require("telescope.pickers")
+        .new({}, {
+          prompt_title = "Harpoon",
+          finder = require("telescope.finders").new_table({
+            results = file_paths,
+          }),
+          previewer = conf.file_previewer({}),
+          sorter = conf.generic_sorter({}),
+        })
+        :find()
+    end
+
+    vim.keymap.set("n", "<leader>ht", function()
+      toggle_telescope(harpoon:list())
+    end, { desc = "telescope" })
   end,
 }
