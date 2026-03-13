84ccbe347f97536f5a51c479e52900a0376da1e2
Brixter Porras <brixterporras@gmail.com>
2026-03-13 17:36:58 +0800

    clean keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -9,21 +9,13 @@ set("n", "<down>", '<cmd>echo "Use j to move down!!"<CR>')
 
 -- better up/down
 set({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
-set({ "n", "x" }, "<Down>", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
 set({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
-set({ "n", "x" }, "<Up>", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
 
 -- Move to window using the <ctrl> hjkl keys
 set("n", "<C-h>", "<C-w>h", { desc = "Go to Left Window", remap = true })
 set("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
 set("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
 set("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window", remap = true })
-
--- Resize window using <ctrl> arrow keys
-set("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
-set("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
-set("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
-set("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
 
 -- Move Lines
 set("n", "<A-j>", "<cmd>execute 'move .+' . v:count1<cr>==", { desc = "Move Down" })
@@ -36,14 +28,6 @@ set("v", "<A-k>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", {
 -- Save and Quit
 set("n", "<leader>w", "<cmd>w<cr>", { silent = false, desc = "Save current buffer" })
 set("n", "<leader>q", "<cmd>q<cr>", { silent = false, desc = "Quit current buffer" })
-
--- Move Lines
-set("n", "<A-j>", "<cmd>execute 'move .+' . v:count1<cr>==", { desc = "Move Down" })
-set("n", "<A-k>", "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = "Move Up" })
-set("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
-set("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
-set("v", "<A-j>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = "Move Down" })
-set("v", "<A-k>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "Move Up" })
 
 -- Clear search and stop snippet on escape
 set("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Escape and Clear hlsearch" })
