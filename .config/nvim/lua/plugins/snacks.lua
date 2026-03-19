08f71fd481dd45f03aebee58e4983cae285c51a1
Brixter Porras <brixterporras@gmail.com>
2026-03-20 02:56:57 +0800

    updates

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -70,53 +70,6 @@ return {
           end
         end,
       })
-
-      vim.api.nvim_create_autocmd("User", {
-        pattern = "VeryLazy",
-        callback = function()
-          -- Setup some globals for debugging (lazy-loaded)
-          _G.dd = function(...)
-            Snacks.debug.inspect(...)
-          end
-          _G.bt = function()
-            Snacks.debug.backtrace()
-          end
-          vim.print = _G.dd -- Override print to use snacks for `:=` command
-
-          -- Create some toggle mappings
-          Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
-          Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
-          Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
-          Snacks.toggle.diagnostics():map("<leader>ud")
-          Snacks.toggle.line_number():map("<leader>ul")
-          Snacks.toggle
-            .option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 })
-            :map("<leader>uc")
-          Snacks.toggle.treesitter():map("<leader>uT")
-          Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background" }):map("<leader>ub")
-          Snacks.toggle.inlay_hints():map("<leader>uh")
-          Snacks.toggle.indent():map("<leader>ug")
-          Snacks.toggle.dim():map("<leader>uD")
-          Snacks.toggle({
-            name = "Git Signs",
-            get = function()
-              return require("gitsigns.config").config.signcolumn
-            end,
-            set = function(state)
-              require("gitsigns").toggle_signs(state)
-            end,
-          }):map("<leader>uG")
-          Snacks.toggle({
-            name = "Highlight color",
-            get = function()
-              return require("nvim-highlight-colors").is_active()
-            end,
-            set = function()
-              require("nvim-highlight-colors").toggle()
-            end,
-          }):map("<leader>uH")
-        end,
-      })
     end,
     -- stylua: ignore
     keys = {
@@ -163,6 +116,39 @@ return {
       { "<leader>sR", function() Snacks.picker.resume() end, desc = "Resume" },
       { "<leader>sq", function() Snacks.picker.qflist() end, desc = "Quickfix List" },
       { "<leader>su", function() Snacks.picker.undo() end, desc = "Undotree" },
+      -- toggle
+      { "<leader>ud", function() Snacks.toggle.diagnostics() end, desc = "Toggle Diagnostics" },
+      { "<leader>ui", function() Snacks.toggle.inlay_hints() end, desc = "Toggle Inlay Hints" },
+      { "<leader>us", function() Snacks.toggle.spell() end, desc = "Toggle Spelling" },
+      { "<leader>ut", function() Snacks.toggle.treesitter() end, desc = "Toggle Treesitter" },
+      
+      { "<leader>ug", 
+        function() 
+          Snacks.toggle({
+            name = "Git Signs",
+            get = function()
+              return require("gitsigns.config").config.signcolumn
+            end,
+            set = function(state)
+              require("gitsigns").toggle_signs(state)
+            end,
+          }) 
+        end,
+        desc = "Toggle Git Signs"
+      },
+      {
+        "<leader>uh",
+        Snacks.toggle({
+          name = "Highlight color",
+          get = function()
+            return require("nvim-highlight-colors").is_active()
+          end,
+          set = function()
+            require("nvim-highlight-colors").toggle()
+          end,
+        }),
+        desc = "Toggle Highlight Colors",
+      }
     },
   },
   {
