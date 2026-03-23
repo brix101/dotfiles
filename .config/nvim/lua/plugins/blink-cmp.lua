43e883c869f843e74890eca4bd8cea6fd6408a1f
Brixter Porras <brixterporras@gmail.com>
2026-03-23 17:01:36 +0800

    updates

--- .config/nvim/lua/plugins/blink-cmp.lua
+++ .config/nvim/lua/plugins/blink-cmp.lua
@@ -26,6 +26,9 @@ return {
           function()
             return require("sidekick").nes_jump_or_apply()
           end,
+          function()
+            return vim.lsp.inline_completion.get()
+          end,
           "fallback",
         },
       },
--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -87,7 +87,7 @@ return {
         options = {
           theme = "auto",
           globalstatus = true,
-          component_separators = { left = "", right = "" },
+          component_separators = { left = "", right = "" },
           section_separators = { left = "█", right = "█" },
         },
         sections = {
